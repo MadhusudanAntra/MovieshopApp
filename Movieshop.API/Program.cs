014826610dc9d73c34fe640a4ca37e516af96533
sharm <sharm@LAPTOP-TALM7IV0>
2022-02-23 16:23:45 -0600

    Add project files.

---
+++ Movieshop.API/Program.cs
@@ -0,0 +1,11 @@
+using Movieshop.API;
+
+CreateHostBuilder().Build().Run();
+
+ static IHostBuilder CreateHostBuilder()
+{
+    return Host.CreateDefaultBuilder().ConfigureWebHostDefaults(webhost => {
+
+        webhost.UseStartup<Startup>();
+    });
+}
