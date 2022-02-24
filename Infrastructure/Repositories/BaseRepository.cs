f7514f2793ccb44fd297bf2d33c420e2fcd9733e
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2022-02-24 13:35:32 -0600

    layered architecture

---
+++ Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Contracts.Repositories;
+namespace Infrastructure.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : class
+    {
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByCondition(Expression<Predicate<T>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Entities;
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : BaseRepository<Movie>, IMovieRepository
+    {
+        public IEnumerable<Movie> Get30HighestRatedMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetByGenre(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
