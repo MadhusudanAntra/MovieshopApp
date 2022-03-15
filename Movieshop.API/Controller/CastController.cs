d162f8cacc247f679832bd72f1f43cb0422dc992
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2022-03-15 15:27:30 -0500

    custom middleware

--- Movieshop.API/Controller/CastController.cs
+++ Movieshop.API/Controller/CastController.cs
@@ -6,6 +6,7 @@ namespace Movieshop.API.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
+
     public class CastController : ControllerBase
     {
         private readonly ICastService _castService;
